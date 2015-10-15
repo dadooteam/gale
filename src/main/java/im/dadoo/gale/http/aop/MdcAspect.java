5c64d95a0fa6307940e02f36dd182c60afb5f0ea
codekitten <codekitten@qq.com>
2015-10-15 17:58:06 +0800

    add sl4j mdc for requesturi

--- src/main/java/im/dadoo/gale/http/aop/MdcAspect.java
+++ src/main/java/im/dadoo/gale/http/aop/MdcAspect.java
@@ -3,28 +3,26 @@ package im.dadoo.gale.http.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import im.dadoo.gale.http.request.GaleRequest;
 
 @Aspect
-@Order(10)
+@Order(1)
 @Component
-public class TimeAspect {
+public class MdcAspect {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(TimeAspect.class);
+  private static final String REQUEST_URI = "RequestUri";
   
   @Around("execution(* im.dadoo.gale.http.server.GaleProcessor.process(..))")
-  public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+  public Object mdc(ProceedingJoinPoint pjp) throws Throwable {
     GaleRequest request = (GaleRequest)pjp.getArgs()[0];
-    
-    long t1 = System.currentTimeMillis();
-    Object ret = pjp.proceed();
-    long t2 = System.currentTimeMillis();
-    LOGGER.info(String.format("uri{%s},delta{%d}ms", request.getUri(), t2 - t1));
-    return ret;
+    MDC.put(REQUEST_URI, request.getUri());
+    Object result = pjp.proceed();
+    MDC.remove(REQUEST_URI);
+    return result;
   }
+  
 }
